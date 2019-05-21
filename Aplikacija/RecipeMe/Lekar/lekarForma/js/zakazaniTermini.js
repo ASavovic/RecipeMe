fdce88e6fb2fe606797fca429c0d0500e239020a
Emma16325 <emilija.simic@elfak.rs>
2019-05-21 13:40:01 +0200

    Sredjena funkcija za eksportovanje u excel tabele za zakazanim pregledima.

--- Aplikacija/RecipeMe/Lekar/lekarForma/js/zakazaniTermini.js
+++ Aplikacija/RecipeMe/Lekar/lekarForma/js/zakazaniTermini.js
@@ -54,4 +54,109 @@
         });
     });
 
+ jQuery(function ($) {
+        $("#exportExcel").click(function () {
+            // parse the HTML table element having an id=exportTable
+            var dataSource = shield.DataSource.create({
+                data: "#dataTable5",
+                schema: {
+                    type: "table",
+                      fields: {
+                        Name: { type: String },
+                        Surname: { type: String },
+                        SSN: { type: String },
+                        Phone: { type: String },
+                        Email: { type: String },
+                        Day: { type: String },
+                        Appointment: { type: String }
+                    }
+                }
+            });
 
+            // when parsing is done, export the data to Excel
+            dataSource.read().then(function (data) {
+                new shield.exp.OOXMLWorkbook({
+                    author: "PrepBootstrap",
+                    worksheets: [
+                        {
+                            name: "Appointments Table",
+                            rows: [
+                                {
+                                    cells: [
+                                        {
+                                            style: {
+                                                bold: true
+                                                
+                                                
+                                            },
+                                            type: String,
+                                            value: "Name"
+                                             
+                                        },
+                                        {
+                                            style: {
+                                                bold: true
+                                            },
+                                            type: String,
+                                            value: "Surname"
+                                            
+                                        },
+                                        {
+                                            style: {
+                                                bold: true
+                                            },
+                                            type: String,
+                                            value: "SSN"
+                                        },
+                                        {
+                                            style: {
+                                                bold: true
+                                            },
+                                            type: String,
+                                            value: "Phone"
+                                        },
+                                        {
+                                            style: {
+                                                bold: true
+                                            },
+                                            type: String,
+                                            value: "Email"
+                                            
+                                        },
+                                        {
+                                            style: {
+                                                bold: true
+                                            },
+                                            type: String,
+                                            value: "Day"
+                                        },
+                                        {
+                                            style: {
+                                                bold: true
+                                            },
+                                            type: String,
+                                            value: "Appointment"
+                                        }
+                                    ]
+                                }
+                            ].concat($.map(data, function(item) {
+                                return {
+                                    cells: [
+                                        { type: String, value: item.Name },
+                                        { type: String, value: item.Surname },
+                                        { type: String, value: item.SSN },
+                                        { type: String, value: item.Phone },
+                                        { type: String, value: item.Email },
+                                        { type: String, value: item.Day },
+                                        { type: String, value: item.Appointment }
+                                    ]
+                                };
+                            }))
+                        }
+                    ]
+                }).saveAs({
+                    fileName: "Appointments"
+                });
+            });
+        });
+    });
